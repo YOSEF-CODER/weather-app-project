138567be5f811d525a9bc0a28591d3c4547d553f
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-04-24 16:49:49 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+<router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+
+import {routerView} from 'vue-router'
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
