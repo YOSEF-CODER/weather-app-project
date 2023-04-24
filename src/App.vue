700bb8218cf16bbafed08ebeb4c39c9fbf2610fe
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-04-24 21:07:26 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
-  
-  <div>
-
-
-<router-view></router-view>
+  <div
+    class="flex flex-col min-h-screen font-Roboto bg-weather-primary"
+  >
+    <SiteNavigation />
+    <RouterView>
+      
+    </RouterView>
   </div>
 </template>
 
 <script setup>
-
-import {routerView} from 'vue-router'
-
+import { RouterView } from "vue-router";
+import SiteNavigation from "./components/SiteNavigation.vue";
 </script>
 
-<style lang="scss" scoped>
+<style>
+.page-enter-active {
+  transition: 600ms ease all;
+}
 
+.page-enter-from {
+  opacity: 0;
+}
 </style>
--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,10 +1,18 @@
 <template>
-    <div>
 
+
+<header class="sticky top-0 shadow bg-weath-lg">
+  <nav class="container flex flex-col items-center gap-4 py-6 text-white sm:flex-row">
+    <router-link >
+    <div class="flex items-center flex-1 gap-3">
+      <i class="text-2xl fa-solid fa-sun"></i>
+      <p class="text-2xl">The Local Weather</p>
     </div>
+    </router-link>
+  </nav>
+</header>
 </template>
 
 <script setup>
 
 </script>
-
